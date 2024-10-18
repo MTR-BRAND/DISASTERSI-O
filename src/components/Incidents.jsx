0d633d52a60891170dd404666d1a41c733856130
MTR-BRAND <mahmudaarafat0077@gmail.com>
2024-10-18 09:50:49 +0600

    first commit

---
+++ src/components/Incidents.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Dashboard from './Dashboard';
+import { FaChalkboard } from 'react-icons/fa6';
+
+function Incidents() {
+
+  return (
+    <>
+      {/* Dashboard View */}
+      <Dashboard/>
+
+    
+    </>
+  );
+}
+
+export default Incidents;
---
+++ src/components/error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <img src="public\img\404.jpg" alt="" />
+  )
+}
+
+export default Error
